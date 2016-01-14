e3eb672b59158efbefab57911a292c4fe23ecbcf
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-01-14 18:36:47 -0200

    core base view

--- app/Core/Providers/CoreServiceProvider.php
+++ app/Core/Providers/CoreServiceProvider.php
@@ -13,7 +13,7 @@ class CoreServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'core');
     }
 
     /**
