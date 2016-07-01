d26af62a19759c1b3fe8cb10070128e8e3c8b95b
Diego Hernandes <diego@hernandev.com>
2016-07-01 20:23:34 -0300

    migrated to a more flexible register of domains

--- app/Domains/DomainsServiceProvider.php
+++ app/Domains/DomainsServiceProvider.php
@@ -2,38 +2,25 @@
 
 namespace App\Domains;
 
+use App\Domains\Users\Providers\UsersServiceProvider;
 use Illuminate\Support\ServiceProvider;
-use App\Domains\Users\Repositories\UsersRepository;
-use App\Domains\Users\Contracts\UsersRepository as UsersRepositoryContract;
-use Migrator\MigratorTrait;
 
 class DomainsServiceProvider extends ServiceProvider
 {
-    // Declare Migrations
-    use MigratorTrait;
-
     /**
-     * Register the service provider.
+     * Main register method.
      */
     public function register()
     {
-        $this->registerUsers();
-
-        $this->registerMigrations();
-    }
-
-    protected function registerMigrations()
-    {
-        $this->migrations([
-
-        ]);
+        $this->registerDomains();
     }
 
     /**
-     * User Domain.
+     * Register domains.
      */
-    protected function registerUsers()
+    protected function registerDomains()
     {
-        $this->app->singleton(UsersRepositoryContract::class, UsersRepository::class);
+        // Users Domain
+        $this->app->register(UsersServiceProvider::class);
     }
 }
