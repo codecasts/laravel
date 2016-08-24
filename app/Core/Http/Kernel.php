66fb92162e56603c5006dcc9a387cb5f4bd45ae2
Diego Hernandes <diego@hernandev.com>
2016-08-24 09:48:30 -0300

    small middleware fix

--- app/Core/Http/Kernel.php
+++ app/Core/Http/Kernel.php
@@ -50,7 +50,7 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'guest' => \App\Core\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
